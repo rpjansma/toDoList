9f31cb6c48980529222a4eafd07d96305923915b
Raul Jansma Paes <rpjansma@gmail.com>
2021-01-09 16:12:46 -0300

    Security configuration

--- src/main/java/com/project/todo/configuration/WebSecurityConfiguration/WebSecurityConfiguration.java
+++ src/main/java/com/project/todo/configuration/WebSecurityConfiguration/WebSecurityConfiguration.java
@@ -1,12 +1,30 @@
 package com.project.todo.configuration.WebSecurityConfiguration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.security.cert.Extension;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoginSuccessHandler loginSuccessHandler;
+
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests().antMatchers("/api/**").authenticated()
@@ -17,9 +35,24 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .authenticationEntryPoint(restAuthenticationEntryPoint)
                 .and()
                 .formLogin()
-                    .loginProcessingUrl("api/login")
-                .sucessHandler(loginSucessHandler)
-                .failureHandler(new SimpleUrlAuthenticationFailureHandler());
+                        .loginProcessingUrl("api/login")
+                    .successHandler(loginSuccessHandler)
+                    .failureHandler(new SimpleUrlAuthenticationFailureHandler());
         http.csrf().ignoringAntMatchers("/api/login").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        String username = "user";
+        String password = "password";
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        String encodedPassword = passwordEncoder().encode(password);
+        manager.createUser(User.withUsername(username).password(encodedPassword).roles("USER").build());
+        return manager;
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
